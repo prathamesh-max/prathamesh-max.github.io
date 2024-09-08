b425e6f1394d46fd1cb8f22563509b55cd5a098d
Prathamesh Yawalkar <pdyawalkar@gmail.com>
2024-09-08 17:51:37 +0200

    ...

--- upload_xl_file_to_webpage.py
+++ upload_xl_file_to_webpage.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the Excel file
-df = pd.read_excel('flight_tickets_data/flights_low_2024-09-08.xlsx')
+df = pd.read_excel('"E:\OneDrive\Desktop\gitinfo\new-project\flight_tickets_data\flights_low_2024-09-08.xlsx"a/flights_low_2024-09-08.xlsx')
 
 # Convert DataFrame to HTML
 html_table = df.to_html(index=False)
