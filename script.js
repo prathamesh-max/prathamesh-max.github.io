bcaaa65f2cdb65741436334765e28bef6908cbec
Prathamesh Yawalkar <pdyawalkar@gmail.com>
2024-09-08 17:24:45 +0200

    Initial commit with website files

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const aboutSection = document.querySelector('#about');
+    aboutSection.style.backgroundColor = '#e0f7fa';
+
+    console.log('JavaScript is working!');
+});
